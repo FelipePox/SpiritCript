86a852cc36fe2e699302119c7d2955ea90f97998
Vinicius Tamanhao <vtamanhao@gmail.com>
2023-05-18 11:25:32 -0300

    dbconnection

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
+const { connectDB } = require("./src/config/dbConn");
+
+const PORT = process.env.PORT || 3500;
+
+//Connection to DB
+connectDB();
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+mongoose.connection.once("open", () => {
+  console.log("connected to mongoDB!");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
